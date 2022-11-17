ba53c409ace4fe22fd80b50e1868cd8ebb62d01c
Dream-gpc <57758908+Dream-gpc@users.noreply.github.com>
2022-11-17 14:45:47 +0800

    Update DeamNet.cpp

--- DeamNet.cpp
+++ DeamNet.cpp
@@ -87,7 +87,7 @@ void pretty_print(const ncnn::Mat& m)
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    if (argc != 2)
     {
         fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
         return -1;
@@ -95,9 +95,7 @@ int main(int argc, char** argv)
     // 读取图片
     const char* imagepath = argv[1];
     cv::Mat m = cv::imread(imagepath, 1);
-    float scale = atof(argv[2]);
-    cout << scale << endl;
-
+   
     if (m.empty())
     {
         fprintf(stderr, "cv::imread %s failed\n", imagepath);
@@ -112,37 +110,39 @@ int main(int argc, char** argv)
 
     ncnn::Net deam;
     // 设置相关参数
-    //ncnn::VulkanDevice vkdev(0);
+    //ncnn::VulkanDevice vkdev(0); 
     //deam.opt.num_threads = 4;
-    deam.opt.use_vulkan_compute = true;
+    deam.opt.use_vulkan_compute = true; //可利用核显加速
     //deam.opt.use_bf16_storage = true;
     //deam.opt.use_packing_layout = true;
     //deam.opt.use_int8_inference = true;
     //deam.opt.use_fp16_packed = true;
    
-    ncnn::Mat in = ncnn::Mat::from_pixels(m.data, ncnn::Mat::PIXEL_BGR2RGB,m.cols, m.rows); 
-  
+    ncnn::Mat in = ncnn::Mat::from_pixels(m.data, ncnn::Mat::PIXEL_BGR2RGB,m.cols, m.rows); //opencv bgr
+    //ncnn::Mat in = ncnn::Mat::from_pixels(m.data, ncnn::Mat::PIXEL_GRAY,m.cols, m.rows); //处理灰度图
+    
     // 导入模型结果以及模型权重
-    if (deam.load_param("DeamNet-real.ncnn.param"))
+    if (deam.load_param("DeamNet-real.ncnn.param")) //DeamNet-grayxx.ncnn.param
         exit(-1);
-    if (deam.load_model("DeamNet-real.ncnn.bin"))
+    if (deam.load_model("DeamNet-real.ncnn.bin"))  //DeamNet-grayxx.ncnn.bin
         exit(-1);
 
   
     // 设置图像预处理相关参数
-    //const float mean_vals[3] = { 104.f, 117.f, 123.f };
-    const float normal_vals[1] = { 1/255.f };
+    //const float mean_vals[3] = { 104.f, 117.f, 123.f }; //去噪和增强模型很少见到减去均值
+    const float normal_vals[3] = { 1/255.f, 1/255.f, 1/255.f }; //彩色图归一化
+    //const float normal_vals[1] = { 1/255.f }; //灰度图归一化
   
     in.substract_mean_normalize(0, normal_vals);
-     //pretty_print(in);
+     //pretty_print(in); 
     ncnn::Extractor ex = deam.create_extractor();
-    //ex.set_light_mode(true);
+    //ex.set_light_mode(true); 
     ex.input("in0", in);
     // 获取模型输出
     ncnn::Mat out;
     ex.extract("out0", out);
-    
-    const float normal_vals1[1] = { 255.f };
+    const float normal_vals1[3] = { 255.f,255.f,255.f, };
+    //const float normal_vals1[1] = { 255.f };//灰度图
     out.substract_mean_normalize(0, normal_vals1);
 
     cv::Mat a(out.h, out.w, CV_8UC3);
@@ -152,10 +152,10 @@ int main(int argc, char** argv)
     auto duration = duration_cast<microseconds>(finish - start);
     auto cost = double(duration.count()) * microseconds::period::num / microseconds::period::den;
 
-    cout << "cost: " << cost << "s." << endl;
+    cout << "cost: " << cost << "s.\n" << endl;
+    cout << "============\n" << endl;
+    cout <<"finish!\n"<< endl;
     cv::imwrite("./test.png", a);
-    printf("============\n");
-    printf("finish!\n");
 
     return 0;
 }
